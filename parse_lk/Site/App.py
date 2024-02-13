9ce413a5c898496add0fddaea22aba3551b9d6e8
MaybeLemon <delemon31@mail.ru>
2024-02-13 13:31:51 +0300

    .

--- parse_lk/Site/App.py
+++ parse_lk/Site/App.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import os
 import json
 from getter import Getter
+from scripts import *
+
 
 app = Flask(__name__)
 JSON_FOLDER = '../json/'
@@ -49,20 +51,9 @@ def json_page(number):
     if os.path.exists(os.path.join(JSON_FOLDER, json_file)):
         with open(os.path.join(JSON_FOLDER, json_file), 'r', encoding='utf-8') as f:
             lessons = json.load(f)
-        unique_names = set()
-        unique_teacher = set()
-        for item in lessons:
-            unique_names.add(item['Название'])
-        for item in lessons:
-            teach = item['Преподаватель']
-            if len(teach.split(', ')) > 1:
-                for x in teach.split(', '):
-                    unique_teacher.add(x)
-            else:
-                unique_teacher.add(item['Преподаватель'])
-        data['unique_teacher'] = sorted(list(unique_teacher), key=lambda x: x)
-        data['unique_names'] = sorted(list(unique_names), key=lambda x: x)
-        data['unique_teacher'].remove('')
+
+        data['unique_names'] = give_unique_names(lessons)
+        data['unique_teacher'] = give_unique_teachers(lessons)
         data['lessons'] = lessons
         if request.method == 'GET':
             return render_template('timesheet.html', data=data)
@@ -70,50 +61,23 @@ def json_page(number):
             if 'sort' in request.form:
                 date = request.form['date']
                 sub_type = request.form['sub_type']
-                if date == "":
-                    if sub_type == 'nontype':
-                        return render_template('timesheet.html', data=data)
-                    else:
-                        data['lessons'] = []
-                        for lesson in lessons:
-                            if lesson['Тип предмета'] == sub_type:
-                                data['lessons'].append(lesson)
-                        return render_template('timesheet.html', data=data)
-                elif sub_type == 'nontype':
-                    data['lessons'] = []
-                    for lesson in lessons:
-                        if date.split('-') == [lesson['Год'], lesson['Месяц'], lesson['День']]:
-                            data['lessons'].append(lesson)
-                    data['lessons'] = sorted(data['lessons'], key=lambda x: int(x['Начало'][:2]))
-                    return render_template('timesheet.html', data=data)
-                else:
-                    data['lessons'] = []
-                    for lesson in lessons:
-                        if lesson['Тип предмета'] == sub_type and date.split('-') == [lesson['Год'], lesson['Месяц'], lesson['День']]:
-                            data['lessons'].append(lesson)
-                    return render_template('timesheet.html', data=data)
+                data = sort_btn(data, date, sub_type, lessons)
+                return render_template('timesheet.html', data=data)
+
             elif 'sub_info' in request.form:
                 sub = request.form['sub_type']
-                data['lessons'] = []
-                for lesson in lessons:
-                    if lesson['Название'] == sub:
-                        data['lessons'].append(lesson)
+                data = sub_info_btn(data, sub, lessons)
                 return render_template('timesheet.html', data=data)
 
             elif 'teacher_sub' in request.form:
-                data['lessons'] = []
                 teacher = request.form['teacher_info']
-                for lesson in lessons:
-                    if teacher in lesson['Преподаватель']:
-                        data['lessons'].append(lesson)
+                data = teacher_sub_btn(data, teacher, lessons)
                 return render_template('timesheet.html', data=data)
-
 
             else:
                 return render_template('timesheet.html', data=data)
     else:
         return render_template('404.html')
-
 
 
 
